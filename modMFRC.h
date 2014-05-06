2f59e08a4a94efe78a821a0a6b129534b79976bb
Spugna85 <fachet85@libero.it>
2014-05-07 01:07:43 +0200

    Update modMFRC.h

--- modMFRC.h
+++ modMFRC.h
@@ -17,10 +17,17 @@
 #define MFRC_RD_OK      1
 #define MFRC_WR_OK      2
 
+#define AUTH_KEYA       1
+#define AUTH_KEYB       2
+#define AUTH_DEFAULT    3
+
+
+extern void setIdxTrailer(uint8_t idx);
+extern void setIdxBlk(uint8_t idx);
 extern void print_tag_sts(uint8_t sts);
-extern void initMFRC();
-extern uint8_t check_rfid(byte write, uint8_t *buf, uint8_t size);
-extern uint8_t MFRC_formatCard();
-extern uint8_t init_rfid();
+extern void initMFRC(byte *kA, byte *kB);
+extern uint8_t check_rfid(uint8_t auth, byte write, uint8_t *buf, uint8_t size);
+extern uint8_t MFRC_formatCard(uint8_t auth);
+extern uint8_t init_rfid(uint8_t auth);
 
 #endif // modMFRC_h
