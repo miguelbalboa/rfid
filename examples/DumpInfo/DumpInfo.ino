90729874011efa50d547c184d805bde9e7f5d246
UnKnoWn-Consortium <UnKnoWn-Consortium@users.noreply.github.com>
2015-01-24 22:03:45 +0000

    Update DumpInfo.ino
    Added pin layouts for ATMEGA32U4-based Arduinos in comment; code updated to account for serial behaviors of ATMEGA32U4-based Arduinos

--- examples/DumpInfo/DumpInfo.ino
+++ examples/DumpInfo/DumpInfo.ino
@@ -48,6 +48,7 @@ MFRC522 mfrc522(SS_PIN, RST_PIN);	// Create MFRC522 instance
 
 void setup() {
 	Serial.begin(9600);		// Initialize serial communications with the PC
+	while (!Serial);                // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
 	SPI.begin();			// Init SPI bus
 	mfrc522.PCD_Init();		// Init MFRC522
 	ShowReaderDetails();	// Show details of PCD - MFRC522 Card Reader details
