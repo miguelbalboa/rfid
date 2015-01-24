993076a0c819da21d5dcacfca871908df9c4c63f
UnKnoWn-Consortium <UnKnoWn-Consortium@users.noreply.github.com>
2015-01-24 23:49:43 +0000

    Update MifareClassicValueBlock.ino
    Added new pin layout info; standardizing format and info

--- examples/MifareClassicValueBlock/MifareClassicValueBlock.ino
+++ examples/MifareClassicValueBlock/MifareClassicValueBlock.ino
@@ -15,16 +15,17 @@
  * 
  * 
  * Typical pin layout used:
- * ------------------------------------------------------------
- *             MFRC522      Arduino       Arduino   Arduino
- *             Reader/PCD   Uno           Mega      Nano v3
- * Signal      Pin          Pin           Pin       Pin
- * ------------------------------------------------------------
- * RST/Reset   RST          9             5         D9
- * SPI SS      SDA(SS)      10            53        D10
- * SPI MOSI    MOSI         11 / ICSP-4   51        D11
- * SPI MISO    MISO         12 / ICSP-1   50        D12
- * SPI SCK     SCK          13 / ICSP-3   52        D13
+ * -----------------------------------------------------------------------------------------
+ *             MFRC522      Arduino       Arduino   Arduino    Arduino          Arduino
+ *             Reader/PCD   Uno           Mega      Nano v3    Leonardo/Micro   Pro Micro
+ * Signal      Pin          Pin           Pin       Pin        Pin              Pin
+ * -----------------------------------------------------------------------------------------
+ * RST/Reset   RST          9             5         D9         RESET/ICSP-5     RST
+ * SPI SS      SDA(SS)      10            53        D10        10               10
+ * SPI MOSI    MOSI         11 / ICSP-4   51        D11        ICSP-4           16
+ * SPI MISO    MISO         12 / ICSP-1   50        D12        ICSP-1           14
+ * SPI SCK     SCK          13 / ICSP-3   52        D13        ICSP-3           15
+ * 
  */
 
 #include <SPI.h>
