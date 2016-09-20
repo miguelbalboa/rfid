8828543db00bea188c032931f3030409266519e5
gallochri <chri@gallochri.com>
2016-09-20 22:21:35 +0200

    Initial custom mfrc522

--- MFRC522.cpp
+++ MFRC522.cpp
@@ -1571,6 +1571,160 @@ void MFRC522::PICC_DumpMifareClassicSectorToSerial(Uid *uid,			///< Pointer to U
 	return;
 } // End PICC_DumpMifareClassicSectorToSerial()
 
+void MFRC522::PICC_DumpMifareClassicBlockToSerial(Uid *uid,			///< Pointer to Uid struct returned from a successful PICC_Select().
+												   MIFARE_Key *key,	///< Key A for the sector.
+												   byte sector,			///< The sector to dump, 0..39.
+                                                    byte block      ///< The Block
+) {
+	MFRC522::StatusCode status;
+	byte firstBlock;		// Address of lowest address to dump actually last block dumped)
+	byte lastBlock;
+    byte no_of_blocks;		// Number of blocks in sector
+	bool isSectorTrailer;	// Set to true while handling the "last" (ie highest address) in the sector.
+
+	// The access bits are stored in a peculiar fashion.
+	// There are four groups:
+	//		g[3]	Access bits for the sector trailer, block 3 (for sectors 0-31) or block 15 (for sectors 32-39)
+	//		g[2]	Access bits for block 2 (for sectors 0-31) or blocks 10-14 (for sectors 32-39)
+	//		g[1]	Access bits for block 1 (for sectors 0-31) or blocks 5-9 (for sectors 32-39)
+	//		g[0]	Access bits for block 0 (for sectors 0-31) or blocks 0-4 (for sectors 32-39)
+	// Each group has access bits [C1 C2 C3]. In this code C1 is MSB and C3 is LSB.
+	// The four CX bits are stored together in a nible cx and an inverted nible cx_.
+	byte c1, c2, c3;		// Nibbles
+	byte c1_, c2_, c3_;		// Inverted nibbles
+	bool invertedError;		// True if one of the inverted nibbles did not match
+	byte g[4];				// Access bits for each of the four groups.
+	byte group;				// 0-3 - active group for access bits
+	bool firstInGroup;		// True for the first block dumped in the group
+
+	// Determine position and size of sector.
+	if (sector < 32) { // Sectors 0..31 has 4 blocks each
+		no_of_blocks = 4;
+		firstBlock = sector * no_of_blocks;
+	}
+	else if (sector < 40) { // Sectors 32-39 has 16 blocks each
+		no_of_blocks = 16;
+		firstBlock = 128 + (sector - 32) * no_of_blocks;
+	}
+	else { // Illegal input, no MIFARE Classic PICC has more than 40 sectors.
+		return;
+	}
+    lastBlock = firstBlock + no_of_blocks;
+    if (block < firstBlock | block > lastBlock){
+        Serial.println(F("ERROR Block out of sector"));
+    }
+	// Dump blocks, highest address first.
+	byte byteCount;
+	byte buffer[18];
+	byte blockAddr;
+	isSectorTrailer = true;
+	for (int8_t blockOffset = no_of_blocks - 1; blockOffset >= 0; blockOffset--) {
+        blockAddr = firstBlock + blockOffset;
+        if (blockAddr == block) {
+            // Sector number - only on first line
+            if (isSectorTrailer) {
+                if (sector < 10)
+                    Serial.print(F("   ")); // Pad with spaces
+                else
+                    Serial.print(F("  ")); // Pad with spaces
+                Serial.print(sector);
+                Serial.print(F("   "));
+            } else {
+                Serial.print(F("       "));
+            }
+            // Block number
+            if (blockAddr < 10)
+                Serial.print(F("   ")); // Pad with spaces
+            else {
+                if (blockAddr < 100)
+                    Serial.print(F("  ")); // Pad with spaces
+                else
+                    Serial.print(F(" ")); // Pad with spaces
+            }
+            Serial.print(blockAddr);
+            Serial.print(F("  "));
+            // Establish encrypted communications before reading the first block
+            if (isSectorTrailer) {
+                status = PCD_Authenticate(PICC_CMD_MF_AUTH_KEY_A, firstBlock, key, uid);
+                if (status != STATUS_OK) {
+                    Serial.print(F("PCD_Authenticate() failed: "));
+                    Serial.println(GetStatusCodeName(status));
+                    return;
+                }
+            }
+            // Read block
+            byteCount = sizeof(buffer);
+            status = MIFARE_Read(blockAddr, buffer, &byteCount);
+            if (status != STATUS_OK) {
+                Serial.print(F("MIFARE_Read() failed: "));
+                Serial.println(GetStatusCodeName(status));
+                continue;
+            }
+            // Dump data
+            for (byte index = 0; index < 16; index++) {
+                if (buffer[index] < 0x10)
+                    Serial.print(F(" 0"));
+                else
+                    Serial.print(F(" "));
+                Serial.print(buffer[index], HEX);
+                if ((index % 4) == 3) {
+                    Serial.print(F(" "));
+                }
+            }
+
+            // Parse sector trailer data
+            if (isSectorTrailer) {
+                c1 = buffer[7] >> 4;
+                c2 = buffer[8] & 0xF;
+                c3 = buffer[8] >> 4;
+                c1_ = buffer[6] & 0xF;
+                c2_ = buffer[6] >> 4;
+                c3_ = buffer[7] & 0xF;
+                invertedError = (c1 != (~c1_ & 0xF)) || (c2 != (~c2_ & 0xF)) || (c3 != (~c3_ & 0xF));
+                g[0] = ((c1 & 1) << 2) | ((c2 & 1) << 1) | ((c3 & 1) << 0);
+                g[1] = ((c1 & 2) << 1) | ((c2 & 2) << 0) | ((c3 & 2) >> 1);
+                g[2] = ((c1 & 4) << 0) | ((c2 & 4) >> 1) | ((c3 & 4) >> 2);
+                g[3] = ((c1 & 8) >> 1) | ((c2 & 8) >> 2) | ((c3 & 8) >> 3);
+                isSectorTrailer = false;
+            }
+
+            // Which access group is this block in?
+            if (no_of_blocks == 4) {
+                group = blockOffset;
+                firstInGroup = true;
+            } else {
+                group = blockOffset / 5;
+                firstInGroup = (group == 3) || (group != (blockOffset + 1) / 5);
+            }
+/*
+            if (firstInGroup) {
+                // Print access bits
+                Serial.print(F(" [ "));
+                Serial.print((g[group] >> 2) & 1, DEC);
+                Serial.print(F(" "));
+                Serial.print((g[group] >> 1) & 1, DEC);
+                Serial.print(F(" "));
+                Serial.print((g[group] >> 0) & 1, DEC);
+                Serial.print(F(" ] "));
+                if (invertedError) {
+                    Serial.print(F(" Inverted access bits did not match! "));
+                }
+            }
+*/
+            if (group != 3 && (g[group] == 1 || g[group] == 6)) { // Not a sector trailer, a value block
+                long value =
+                        (long(buffer[3]) << 24) | (long(buffer[2]) << 16) | (long(buffer[1]) << 8) | long(buffer[0]);
+                Serial.print(F(" Value=0x"));
+                Serial.print(value, HEX);
+                Serial.print(F(" Adr=0x"));
+                Serial.print(buffer[12], HEX);
+            }
+            Serial.println();
+        }
+    }
+	return;
+} // End PICC_DumpMifareClassicSectorToSerial()
+
 /**
  * Dumps memory contents of a MIFARE Ultralight PICC.
  */
