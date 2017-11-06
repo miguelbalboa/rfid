5eaa55626234e542b87d7c9fa41529ec862ce644
Mads Mogensen <lort256c@gmail.com>
2017-11-06 22:18:19 +0100

    Changed to uint8_t and loop stops at 255

--- examples/AccessControl/AccessControl.ino
+++ examples/AccessControl/AccessControl.ino
@@ -460,7 +460,7 @@ uint8_t findIDSLOT( byte find[] ) {
 ///////////////////////////////////////// Find ID From EEPROM   ///////////////////////////////////
 boolean findID( byte find[] ) {
   uint8_t count = EEPROM.read(0);     // Read the first Byte of EEPROM that
-  for ( uint16_t i = 1; i <= count; i++ ) {    // Loop once for each EEPROM entry
+  for ( uint8_t i = 1; i < count; i++ ) {    // Loop once for each EEPROM entry
     readID(i);          // Read an ID from EEPROM, it is stored in storedCard[4]
     if ( checkTwo( find, storedCard ) ) {   // Check to see if the storedCard read from EEPROM
       return true;
