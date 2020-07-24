39c90621b020b7f5ebcc2670b58c7a3a0dbe9863
Rotzbua <Rotzbua@users.noreply.github.com>
2020-07-24 12:40:45 +0200

    fix typos

--- examples/Ntag216_AUTH/Ntag216_AUTH.ino
+++ examples/Ntag216_AUTH/Ntag216_AUTH.ino
@@ -1,13 +1,17 @@
-//This example show how you can get Authenticated by the NTAG213,215,216 by default the tags are unprotected in order to protect them we need to write 4 different values:
-// Using mfrc522.MIFARE_Ultralight_Write(PageNum, Data, #Databytes))
-//1.- we need to write the 32bit passWord to page 0xE5 !for ntag 213 and 215 page is different refer to nxp documentation!
-//2.- Now Write the 16 bits pACK to the page 0xE6 use the 2 high bytes like this: pACKH + pACKL + 00 + 00 after an authentication the tag will return this secret bytes
-//3.- Now we need to write the first page we want to protect this is a 1 byte data in page 0xE3 we need to write 00 + 00 + 00 + firstPage  all pages after this one are write protected
-// Now WRITE protection is ACTIVATED so we need to get authenticated in order to write the last data
-//4.- Finally we need to write an access record in order to READ protect the card this step is optional only if you want to read protect also write 80 + 00 + 00 + 00 to 0xE4
-//After completeing all these steps you will nee to authentiate first in order to read or write ant page after the first page you selected to protect
-//To disengage proection just write the page (0xE3) to 00 + 00 + 00 + FF that going to remove all protection
-//Made by GARGANTUA from RoboCreators.com & paradoxalabs.com
+/**
+ * This example show how you can get Authenticated by the NTAG213,215,216. By default the tags are unprotected in order to protect them we need to write 4 different values:
+ * Using mfrc522.MIFARE_Ultralight_Write(PageNum, Data, #Databytes))
+ * 1.- We need to write the 32bit passWord to page 0xE5 !for ntag 213 and 215 page is different refer to nxp documentation!
+ * 2.- Now Write the 16 bits pACK to the page 0xE6 use the 2 high bytes like this: pACKH + pACKL + 00 + 00 after an authentication the tag will return this secret bytes
+ * 3.- Now we need to write the first page we want to protect this is a 1 byte data in page 0xE3 we need to write 00 + 00 + 00 + firstPage  all pages after this one are write protected
+ *     Now WRITE protection is ACTIVATED so we need to get authenticated in order to write the last data
+ * 4.- Finally we need to write an access record in order to READ protect the card this step is optional only if you want to read protect also write 80 + 00 + 00 + 00 to 0xE4
+ * After completing all these steps you will nee to authenticate first in order to read or write ant page after the first page you selected to protect.
+ * To disengage protection just write the page (0xE3) to 00 + 00 + 00 + FF that going to remove all protection.
+ * 
+ * @author GARGANTUA from RoboCreators.com & paradoxalabs.com
+ * @license Released into the public domain.
+ */
 
 #include <SPI.h>
 #include <MFRC522.h>
@@ -52,7 +56,7 @@ void loop() {
   //Serial.print("CHG BLK: ");
   //Serial.println(mfrc522.MIFARE_Ultralight_Write(0xE3, WBuff, 4));  //How to write to a page
 
-  mfrc522.PICC_DumpMifareUltralightToSerial(); //This is a modifier dunp just cghange the for cicle to < 232 instead of < 16 in order to see all the pages on NTAG216
+  mfrc522.PICC_DumpMifareUltralightToSerial(); // This is a modifier dump just change the for circle to < 232 instead of < 16 in order to see all the pages on NTAG216.
 
   delay(3000);
 }
