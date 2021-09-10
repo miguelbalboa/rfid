6a88cda3e3aee793605f62fde793f0bad145b6fc
espinosa <espinosa@biblioeteca.com>
2021-09-10 22:51:39 +0200

    Fix error

--- src/MFRC522.cpp
+++ src/MFRC522.cpp
@@ -102,6 +102,43 @@ byte MFRC522::PCD_ReadRegister(	PCD_Register reg	///< The register to read from.
 } // End PCD_ReadRegister()
 
 /**
+ * Reads a number of bytes from the specified register in the MFRC522 chip.
+ * The interface is described in the datasheet section 8.1.2.
+ */
+void MFRC522::PCD_ReadRegister(	PCD_Register reg,	///< The register to read from. One of the PCD_Register enums.
+								byte count,			///< The number of bytes to read
+								byte *values,		///< Byte array to store the values in.
+								byte rxAlign		///< Only bit positions rxAlign..7 in values[0] are updated.
+								) {
+	if (count == 0) {
+		return;
+	}
+	//Serial.print(F("Reading ")); 	Serial.print(count); Serial.println(F(" bytes from register."));
+	byte address = 0x80 | reg;				// MSB == 1 is for reading. LSB is not used in address. Datasheet section 8.1.2.3.
+	byte index = 0;							// Index in values array.
+	SPI.beginTransaction(SPISettings(MFRC522_SPICLOCK, MSBFIRST, SPI_MODE0));	// Set the settings to work with SPI bus
+	digitalWrite(_chipSelectPin, LOW);		// Select slave
+	count--;								// One read is performed outside of the loop
+	SPI.transfer(address);					// Tell MFRC522 which address we want to read
+	if (rxAlign) {		// Only update bit positions rxAlign..7 in values[0]
+		// Create bit mask for bit positions rxAlign..7
+		byte mask = (0xFF << rxAlign) & 0xFF;
+		// Read value and tell that we want to read the same address again.
+		byte value = SPI.transfer(address);
+		// Apply mask to both current value of values[0] and the new data in value.
+		values[0] = (values[0] & ~mask) | (value & mask);
+		index++;
+	}
+	while (index < count) {
+		values[index] = SPI.transfer(address);	// Read value and tell that we want to read the same address again.
+		index++;
+	}
+	values[index] = SPI.transfer(0);			// Read the final byte. Send 0 to stop reading.
+	digitalWrite(_chipSelectPin, HIGH);			// Release slave again
+	SPI.endTransaction(); // Stop using the SPI bus
+} // End PCD_ReadRegister()
+
+/**
  * Sets the bits given in mask in register reg.
  */
 void MFRC522::PCD_SetRegisterBitMask(	PCD_Register reg,	///< The register to update. One of the PCD_Register enums.
